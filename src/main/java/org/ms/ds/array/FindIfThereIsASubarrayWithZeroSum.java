0f439ae294d878d7fda27e743e750de2d34893f4
Manish Sahu <manishsahu009@gmail.com>
2020-06-29 01:09:05 +0530

    Print all subarrays with 0 sum

---
+++ src/main/java/org/ms/ds/array/FindIfThereIsASubarrayWithZeroSum.java
@@ -0,0 +1,17 @@
+package org.ms.ds.array;
+
+/*
+
+If we consider all prefix sums, we can
+notice that there is a subarray with 0
+sum when :
+1) Either a prefix sum repeats or
+2) Or prefix sum becomes 0.
+
+ */
+
+public class FindIfThereIsASubarrayWithZeroSum {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/org/ms/ds/hashing/PrintAllSubarraysWithZerSum.java
@@ -0,0 +1,42 @@
+package org.ms.ds.hashing;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class PrintAllSubarraysWithZerSum {
+    public static void main(String[] args) {
+        PrintAllSubarraysWithZerSum print = new PrintAllSubarraysWithZerSum();
+        Integer[] elements = {3, 4, -7, 3, 1, 3, 1, -4, -2, -2};
+        // prefix sum {3, 7, 0, 3, 4, 7, 8, 4, 2, 0}
+        print.printZeroSumSubarray(elements);
+    }
+
+    void printZeroSumSubarray(Integer[] elements) {
+        // here in 2 condition subarray will be zero
+        //1. if sum is zero
+        //2. if if any key(sum) repeates (x(1st occurance), ....,x(2nd occurance)) => index of x+1 to x subarray will have sum as zero
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        int sum = 0;
+        insert(map,0,-1); //
+         for (int i = 0; i < elements.length;i++) {
+            sum=sum+elements[i];
+            if(map.containsKey(sum)){
+                List<Integer> list=map.get(sum);
+                for(Integer value:list){
+                    System.out.println("key(" +sum+") index : "+ (value+1) +" to "+ i);
+                }
+            }
+            insert(map,sum,i);
+        }
+    }
+
+    void insert(Map<Integer, List<Integer>> map, Integer sum, Integer ele) {
+        if (!map.containsKey(sum)) {
+            map.put(sum, new ArrayList<Integer>());
+        }
+        map.get(sum).add(ele);
+    }
+
+}
